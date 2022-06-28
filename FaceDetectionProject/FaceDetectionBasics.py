4faca5df67d0f6fc17aab33799a36acbc398cc81
Shangjie Du <sd3497@columbia.edu>
2022-06-29 06:02:34 +0800

    update FaceDetectionProject

---
+++ FaceDetectionProject/FaceDetectionBasics.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture(0)
+pTime = time.time()
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            ih, iw, ic = img.shape
+            bboxC = detection.location_data.relative_bounding_box
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+
+    cTIme = time.time()
+    fps = 1 / (cTIme - pTime)
+    pTime = cTIme
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 2)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
